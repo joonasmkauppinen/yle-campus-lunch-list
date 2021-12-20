2fa4663553c34d7ce6ecce2a7998e51584dfa4fd
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2021-12-20 19:24:46 +0200

    refactor(TitleH1): simplify component

--- components/TitleH1/TitleH1.tsx
+++ components/TitleH1/TitleH1.tsx
@@ -1,21 +1,9 @@
-/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
 
-interface TitleH1Props {
-  children: string;
-}
-
-export const TitleH1 = ({ children }: TitleH1Props) => {
-  return (
-    <h1
-      css={{
-        color: '#1B1B1B',
-        fontSize: 34,
-        margin: 0,
-        padding: 0,
-        marginBottom: 40,
-      }}
-    >
-      {children}
-    </h1>
-  );
-};
+export const TitleH1 = styled.h1({
+  color: '#1B1B1B',
+  fontSize: 34,
+  margin: 0,
+  padding: 0,
+  marginBottom: 40,
+});
