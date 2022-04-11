5e162e591d7a73f1596cb85dff232e5926aba87e
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-04-11 21:09:15 +0300

    fix(api/current-day-menus): update revalidation token env variable name

--- pages/api/revalidate/current-day-menus.ts
+++ pages/api/revalidate/current-day-menus.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.NEXT_APP_REVALIDATION_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
