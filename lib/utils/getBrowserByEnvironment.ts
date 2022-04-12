e82557303c4dcff1eacdeb7320115e4e0a7c2d04
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-04-12 19:44:13 +0300

    fix: import only the required params from `chrome-aws-lambda` to reduce the serverless function size

--- lib/utils/getBrowserByEnvironment.ts
+++ lib/utils/getBrowserByEnvironment.ts
@@ -2,12 +2,14 @@ import playwright from 'playwright-core';
 
 export const getBrowserByEnvironment = async () => {
   if (process.env.AWS_LAMBDA_FUNCTION_NAME !== undefined) {
-    const chromium = (await import('chrome-aws-lambda')).default;
+    const args = (await import('chrome-aws-lambda')).default.args;
+    const executablePath = (await import('chrome-aws-lambda')).default.executablePath;
+    const headless = (await import('chrome-aws-lambda')).default.headless;
 
     return playwright.chromium.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
+      args: args,
+      executablePath: await executablePath,
+      headless: headless,
     });
   } else {
     return playwright.chromium.launch();
