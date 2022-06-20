c19679cb3a742346c19debbda7eddae8d841f9b4
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-06-20 10:32:38 +0300

    feat: add `next-pwa` config and static assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,14 +58,33 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="https://yle-campus-lunch-list.vercel.app/icons/icon_192x192.png"
         />
         {/* <!-- apple splash screen images --> */}
-        {/* <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> 
-        */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple_splash_2048.png"
+          sizes="2048x2732"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple_splash_1668.png"
+          sizes="1668x2224"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple_splash_1536.png"
+          sizes="1536x2048"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple_splash_1125.png"
+          sizes="1125x2436"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple_splash_1242.png"
+          sizes="1242x2208"
+        />
+        <link rel="apple-touch-startup-image" href="/icons/apple_splash_750.png" sizes="750x1334" />
+        <link rel="apple-touch-startup-image" href="/icons/apple_splash_640.png" sizes="640x1136" />
       </Head>
       <PageContainer>
         <Component {...pageProps} />
